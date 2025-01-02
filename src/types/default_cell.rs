f11704301f1e7c54e94928e824e4e07303efb1aa
Frank Moreno @kranfix <frankmoreno1993@gmail.com>
2025-01-01 20:18:39 -0500

    only needed methos for DefaultCell

--- src/types/default_cell.rs
+++ src/types/default_cell.rs
@@ -1,23 +1,13 @@
-use std::{
-  cell::{Cell, Ref, RefCell, RefMut},
-  sync::LazyLock,
-};
+use std::cell::{Cell, LazyCell, Ref, RefCell, RefMut};
 
 #[derive(Debug, Default)]
 pub struct DefaultCell<T> {
   // TODO(kranfix): remove when all the API of LazyCell is stable
   initialized: Cell<bool>,
-  lazy: LazyLock<RefCell<T>>,
+  lazy: LazyCell<RefCell<T>>,
 }
 
 impl<T: Default> DefaultCell<T> {
-  pub fn new() -> Self {
-    DefaultCell::default()
-  }
-
-  pub fn is_initialized(&self) -> bool {
-    self.initialized.get()
-  }
   pub fn lazy_borrow(&self) -> Option<Ref<'_, T>> {
     match self.initialized.get() {
       true => Some(Ref::map(self.lazy.borrow(), |v| v)),
@@ -25,19 +15,9 @@ impl<T: Default> DefaultCell<T> {
     }
   }
 
-  pub fn borrow(&self) -> Ref<'_, T> {
-    self.initialized.set(true);
-    Ref::map(self.lazy.borrow(), |v| v)
-  }
   pub fn borrow_mut(&self) -> RefMut<'_, T> {
     self.initialized.set(true);
     RefMut::map(self.lazy.borrow_mut(), |v| v)
-  }
-  pub fn update<F, R>(&self, f: F) -> R
-  where
-    F: FnOnce(&mut T) -> R,
-  {
-    f(&mut self.borrow_mut())
   }
 }
 
